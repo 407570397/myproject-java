7a6353f548876519343f99e5c33e24f2a5639372
407570397 <92625930+407570397@users.noreply.github.com>
2021-11-30 15:24:28 +0800

    Update UsingCoin.java

--- UsingCoin.java
+++ UsingCoin.java
@@ -44,3 +44,111 @@ public class UsingCoin {
         }
     }
 }
+
+package project;
+import java.util.Scanner;
+public class UsingCoin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        Boolean end = false;
+        int aPrice = 15;
+        int bPrice = 20;
+        int cPrice = 25;
+        int[] Prices = {15, 20, 25};
+        System.out.println(Prices.length);
+        for (int i = 0; i < Prices.length; i++) {
+            System.out.println(Prices[i]);
+        }
+        while (!end){
+            System.out.println("Please put your coin(1/5/10):  Tatol:" + total);
+            String s = scanner.next();
+            switch (s){
+                case "1":
+                case "5":
+                case "10":
+                    int n = Integer.parseInt(s);
+                    total+=n;
+                    break;
+                case "a":
+                    if (total>=aPrice){
+                        System.out.println("DON!");
+                        total-=aPrice;
+                    }else {
+                        System.out.println("BEEP!");
+                    }
+                    break;
+                case "b":
+                    if (total>=bPrice){
+                        System.out.println("DON!");
+                        total-=bPrice;
+                    }else {
+                        System.out.println("BEEP!");
+                    }
+                    break;
+                case "c":
+                    if (total>=cPrice){
+                        System.out.println("DON!");
+                        total-=cPrice;
+                    }else {
+                        System.out.println("BEEP!");
+                    }
+                    break;
+            }
+        }
+    }
+}
+package project;
+import java.util.Scanner;
+public class UsingCoin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        Boolean end = false;
+        int[] Prices = {15, 20, 25};
+        System.out.println(Prices.length);
+        for (int i = 0; i < Prices.length; i++) {
+            System.out.println(Prices[i]);
+        }
+        while (!end){
+            System.out.println("Please put your coin(1/5/10):  Tatol:" + total);
+            String s = scanner.next();
+            switch (s) {
+                case "1":
+                case "5":
+                case "10":
+                    int n = Integer.parseInt(s);
+                    total += n;
+                    break;
+                case "e":
+                    int index = s.charAt(0) - 101;
+                    if (total >= Prices[index]) {
+                        System.out.println("DON!");
+                        total -= Prices[index];
+                        break;
+                    } else {
+                        System.out.println("BEEP!");
+                        break;
+                    }
+                default:
+                    System.out.println("Wrong answer!");
+                    break;
+            }
+        }
+    }
+}
+package project;
+
+import java.util.Random;
+
+public class dice {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] answer = {1, 2, 3, 4, 5, 6};
+        for (int i = 0; i < 10; i++) {
+            int dice = random.nextInt(6)+1;
+            System.out.println(dice);
+        }
+       
+    }
+}
