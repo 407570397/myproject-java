0b41794da9574c151e6a99272117a119ac80be76
407570397 <92625930+407570397@users.noreply.github.com>
2021-12-14 13:21:56 +0800

    Update UsingCoin.java

--- UsingCoin.java
+++ UsingCoin.java
@@ -212,3 +212,17 @@ public class main {
         }
     }
 }
+package project;
+
+public class main {
+    public static void main(String[] args) {
+
+        char[] types = {'c', 'd', 'h', 's'};
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 13; j++) {
+                System.out.print((j+1) + "" + types[i] + " ");
+                }
+            System.out.println();
+        }
+    }
+}
