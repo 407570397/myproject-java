40c9358b0a6f9af1360650946675f06343bb2487
407570397 <92625930+407570397@users.noreply.github.com>
2022-01-04 14:50:55 +0800

    Update UsingCoin.java

--- UsingCoin.java
+++ UsingCoin.java
@@ -371,3 +371,19 @@ public class Random {
         }
     }
 }
+
+//0104
+public class pp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i>0 && i!=4 && j!=0 && j!=5){
+                    System.out.print(" ");
+                }else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
